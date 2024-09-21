1d246c690321cbc8634af8f4a7501b1ebbafcbd0
= <=>
2024-09-21 18:10:29 -0300

    feat: add project

---
+++ problem_1/index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int maxIntToCompare = 1000;
+    int sum = 0;
+
+
+    for(int i = 0; i < maxIntToCompare; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0) sum += i;
+    }
+
+    std::cout << sum;
+}
---
+++ problem_2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() 
+{
+    int lastTwoFibonacci[2] = {1, 2};
+    int maxFibonacci = 4000000; /* 4 million */
+    int sum = 2;
+
+    while (true)
+    {
+        int tmp = lastTwoFibonacci[1];
+        int currFibonacci = lastTwoFibonacci[0] + lastTwoFibonacci[1];
+
+        lastTwoFibonacci[0] = tmp;
+        lastTwoFibonacci[1] = currFibonacci;
+
+        if (currFibonacci > maxFibonacci) break;
+        if (currFibonacci % 2 == 0) sum += currFibonacci;
+    }
+
+    std::cout << sum << std::endl;
+}
