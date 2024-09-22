d4d34dbf4ecfbc278ca81c93bf94be3539ee759b
= <=>
2024-09-21 23:27:31 -0300

    problem 3

---
+++ problem_3/main.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+// Any divisor of a number greater than its square root is a multiple of a smaller divisor
+// that has already been tested
+
+bool checkIfIsPrime(int n) 
+{
+    for (int i = 2; i < sqrt(n); i++)
+    {
+        if (n % i == 0) return false; 
+    }
+
+    return true;
+}
+
+int main()
+{
+    long n = 600851475143;
+    int potentiallyPrimeNumber = sqrt(n);
+
+    while (true)
+    {
+        if (n % potentiallyPrimeNumber == 0 && checkIfIsPrime(potentiallyPrimeNumber)) break; 
+
+        potentiallyPrimeNumber -= 1;
+    }
+    
+    cout << potentiallyPrimeNumber << endl;
+}
